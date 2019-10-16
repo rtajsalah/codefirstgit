468abfc3ce18bca6c3241e5b418df7618f29e6d2
rtaj <rtaj123@hotmail.co.uk>
2019-10-16 19:55:40 +0100

    first commit

---
+++ my_name.py
@@ -0,0 +1,19 @@
+print ("What's your name?")
+name = input()
+print("Hello "+name)
+
+a=int(input("Input number 1: "))
+b=int(input("Input number 2: "))
+
+def hello_world(a,b):
+    c = a + b
+    print(str(c))
+
+hello_world(a,b)
+
+empty_list = []
+list_of_numbers = [0, 1, 2, 3, 4, 5]
+my_favourite_fruits = ["pineapples" , "oranges", "bananas"]
+mixed_list = [15, "sunshine", "jumper", 4, "sky"]
+
+print(my_favourite_fruits[1:2])    #first value for the postion and second for the nth element
